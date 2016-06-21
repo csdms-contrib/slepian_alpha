73c1dabf742b4da76435a4b562b639cb2cbe357b
Frederik J. Simons <[fjsimons@gmail.com]>
2016-06-21 17:24:15 -0400

    XTRAXIS testing on Matlab 8 and 9, providing of small example

--- xtraxis.m
+++ xtraxis.m
@@ -24,9 +24,25 @@ function [ax,xl,yl]=xtraxis(ah,xti,xtil,xlab,yti,ytil,ylab)
 % specify 'xlim' (using the parent values) again, at the end.
 % A recent example of the linear case is in SWREGIONS2D. 
 %
+% EXAMPLE
+%
+% clf; mnsize=[301 277]; spun=[1500 1380];
+% [fx,fy,fxnyq,fynyq]=fftaxis(mnsize,mnsize,spun);
+% kc11=[-max(fx) max(fy)]*2*pi;
+% kcmn=[-min(fx) min(fy)]*2*pi;
+% imagefnan(kc11,kcmn,randn(mnsize))
+% xl=xlabel('k_x (rad/km)');
+% yl=ylabel('k_y (rad/km)');
+% lambda=[-80 -20 -10 10 20 80];
+% longticks(gca,2)
+% ax=xtraxis(gca,2*pi./lambda,abs(lambda),'wavelength (km)',...
+%         2*pi./lambda,abs(lambda),'wavelength (km)');
+% longticks(ax,2)
+%
 % See LAXIS, XTRAXIS1D, XTRAXIS2D
 %
-% Last modified by fjsimons-at-alum.mit.edu, 06/08/2015
+% Tested on 8.3.0.532 (R2014a) and 9.0.0.341360 (R2016a)
+% Last modified by fjsimons-at-alum.mit.edu, 06/21/2016
 
 defval('ah',gca)
 defval('xti',[])
@@ -58,9 +74,9 @@ yll=ylim(ah);
 set(ah,'box','off')
 set(ax,'XTick',xti,'XTickLabel',xtil,...
        'xlim',xll,'ylim',yll,...
-       'ytick',yti,'ytickLabel',ytil,...
+       'ytick',yti,'YTickLabel',ytil,...
        'Xgrid','off','Ygrid','off',...
-       'XaxisLabel','top','YaxisLabel','right',...
+       'XAxisLocation','top','YAxisLocation','right',...
        'XScale',get(ah,'XScale'),'YScale',get(ah,'YScale'),...
        'box','off','Visible','on','Color','none');
 % For some reason these are tricky when there's auto
