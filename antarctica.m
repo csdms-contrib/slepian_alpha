109a6feb6a0c5cf887de2febce88f1cd9882b73a
Frederik J. Simons <fjsimons@gmail.com>
2016-07-01 09:03:16 -0400

    Editorial minor cleanup of ANTARCTICA

--- antarctica.m
+++ antarctica.m
@@ -7,13 +7,12 @@ function varargout=antarctica(res,buf,rotb)
 %
 % INPUT:
 %
-% res      0 The standard, default values,
-%          N Splined values at N times the resolution,
-%          'rotated' Use this feature to determine if this region is
-%          rotated (it is). We use this in e.g. GLMALPHA to determine
-%          whether to rotate the eigenfunctions after finding them. You
-%          will get a logical as output.
-% buf      Distance in degrees that the region outline will be enlarged
+% res      0 The standard, default values
+%          N Splined values at N times the resolution
+%          'rotated' to determine if this region is rotated (it is). We
+%          use this in e.g. GLMALPHA to determine whether to rotate the
+%          eigenfunctions after finding them. You get a logical as output.
+% buf      Distance in degrees by which the region outline will be enlarged
 %          by BUFFERM, not necessarily integer, possibly negative
 %          [default: 0]
 % rotb     0 You want the coordinates on the equator for mathematical 
@@ -30,11 +29,11 @@ function varargout=antarctica(res,buf,rotb)
 % See also PLM2ROT, GEOBOXCAP, KLMLMP2ROT, GLMALPHA
 % 
 % Last modified by charig@princeton.edu, 06/24/2016
+% Last modified by fjsimons@princeton.edu, 07/01/2016
 
 defval('res',0)
 defval('buf',0)
 defval('rotb',0)
-
 
 if ~isstr(res)
     
@@ -59,7 +58,8 @@ if ~isstr(res)
 
   % Do we return rotated coordinates?
   if rotb==1
-     [thetap,phip,rotmats]=rottp((90-XY(:,2))*pi/180,XY(:,1)/180*pi,-lonc*pi/180,latc*pi/180,0);
+     [thetap,phip,rotmats]=rottp((90-XY(:,2))*pi/180,XY(:,1)/180*pi,...
+				 -lonc*pi/180,latc*pi/180,0);
      XY = [phip*180/pi 90-thetap*180/pi];
   end
 
@@ -74,14 +74,13 @@ if ~isstr(res)
 elseif strcmp(res,'rotated')
     % Return a 1 flag as output, indicating the region is a rotated region
     varargout={1};
-
 elseif strcmp(res,'demo1')
     % Make a simple plot
     XY=antarctica();
     plot(XY(:,1),XY(:,2),'k-'); axis equal; grid on
     axis([-30-lonc 30-lonc -30 20])
-
 end
+
 
 
 
