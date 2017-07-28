683beaa099715b97e8af56a8dcaba6525ec39322
Frederik J. Simons <fjsimons@gmail.com>
2017-07-27 22:55:54 -0400

    Additional functionality and bug fixes

--- cbarticks.m
+++ cbarticks.m
@@ -1,28 +1,36 @@
 function cbarticks(cb,cax,tint,pos)
 % CBARTICKS(cb,cax,tint,pos)
 %
-% Transforms the tick marks and labels to agree with
-% what's plotted in the figure.
+% Transforms axis tick marks and labels to other values, e.g. for a
+% colorbar that indexes an IMAGE type figure, so that the ticks, which
+% would otherwise be marking color indices, agree with the actual values
+% of what's being plotted in the figure and for which the axis is a
+% colorbar. 
 %
 % INPUT:
 %
-% cb       Handle to the colorbar
+% cb       Handle to an axis, especially, a colorbar from ADDCB
 % cax      Minimum and maximum of the color range
-% tint     Tick interval
-% pos      'hor' or 'vert'
+% tint     If a single number: tick INTERVAL, cax(1):tint:cax(2)
+%          If two or more numbers: actual ticks themselves
+% pos      'hor' or 'vert' position of the axis handle cb
 %
 % SEE ALSO:
 %
 % ADDCB, IMAGEF, IMAGEFDIR, IMAGEFNAN
 % 
-% Last modified by fjsimons-at-alum.mit.edu, 06/19/2008
 % Last modified by charig-at-princeton.edu, 02/05/2016
+% Last modified by fjsimons-at-alum.mit.edu, 07/26/2017
 
 defval('tint',20)
 defval('pos','hor')
 
-% Make sure the last one makes it regardless of tint
-tix=unique([cax(1):tint:cax(2) cax(2)]);
+if length(tint)==1
+  % Make sure the last one makes it regardless of tint
+  tix=unique([cax(1):tint:cax(2) cax(2)]);
+else
+  tix=tint;
+end
 
 if strcmp(pos,'hor')
   set(cb,'Xtick',scale(tix,get(cb,'xlim')))
