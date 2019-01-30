8026e728ea75a8c2cb5a5d9d6672c5908c0b35b6
Frederik J. Simons <fjsimons@gmail.com>
2019-01-30 11:17:56 -0500

    Returns the ith column of a matrix

--- kindeks.m
+++ kindeks.m
@@ -1,12 +1,18 @@
 function x=kindeks(M,i)
 % x=KINDEKS(M,i)
 %
-% Returns the COLUMN i of a matrix M.
-% Returns the ith 2-nd dimension of a 3D matrix.
+% Returns the COLUMN(s) i of a matrix M.
+% Returns the ith 2-nd dimension(s) of a matrix M.
 %
-% See also INDEKS, RINDEKS.
+% EXAMPLE:
 %
-% Last modified by fjsimons-at-alum.mit.edu, 09/28/2006
+% M=rand(randij(12))
+% witsj=randi(size(M,2))
+% kindeks(M,witsj)
+%
+% See also INDEKS, RINDEKS, TINDEKS, DINDEKS, SQUEEZE.
+%
+% Last modified by fjsimons-at-alum.mit.edu, 01/30/2019
 
-x=M(:,i,:);
+x=M(:,i(:),:);
 
