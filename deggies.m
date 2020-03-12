c9b46d36d77d4e57655a85e57bafcb66e860572e
Frederik J. Simons <fjsimons@gmail.com>
2020-03-12 17:18:44 -0400

    Puts degree signs on axis ticklabels

--- deggies.m
+++ deggies.m
@@ -1,57 +1,71 @@
-function deggies(ah,varargin)
-% DEGGIES(ah)
-% DEGGIES(ah,1) % Only x
-% DEGGIES(ah,2) % Only y
+function deggies(ah,swit)
+% DEGGIES(ah,swit)
 %
-% This function puts degree signs on the ticklabels of the
-% axes with handle 'ah'. Takes vectors!
+% Puts degree signs on ticklabels
 %
-% See also DEGS
+% INPUT:
 %
-% Last modified by fjsimons-at-alum.mit.edu, 12/26/2006
+% ah       The axis handle(s)
+% swit     1 Only on the x-axis
+%          2 Only on the y-axis
+%          3 On both the x and the y axes (default)
+%
+% See also DEGS, a Unix script to fix a PostScript error
+% 
+% Tested on 8.3.0.532 (R2014a)
+%
+% Last modified by fjsimons-at-alum.mit.edu, 03/12/2020
 
 defval('ah',gca)
+defval('swit',3)
 
-if nargin>1
-  swit=varargin{1};
-else 
-  swit=3;
-end
-
+% Apply to all the axis handles necessary
 for index=1:length(ah)
   XTL=get(ah(index),'XTickLabel');
   YTL=get(ah(index),'YTickLabel');
 
+  % ASCII codes for special things
   space=32;
   circ=176;
 
-  % This is for new Matlab, which renders badly in postscript
-  XTL=[XTL repmat(str2mat(space),size(XTL,1),1)];
-  YTL=[YTL repmat(str2mat(space),size(YTL,1),1)];
+  if verLessThan('matlab', '9.0.0')
+    % This is what renders badly in PostScript
+    XTL=[XTL repmat(str2mat(space),size(XTL,1),1)];
+    YTL=[YTL repmat(str2mat(space),size(YTL,1),1)];
 
-  mnx=size(XTL);
-  mny=size(YTL);
+    mnx=size(XTL);
+    mny=size(YTL);
 
-  indx=sum(XTL==' ',2);
-  indy=sum(YTL==' ',2);
+    indx=sum(XTL==' ',2);
+    indy=sum(YTL==' ',2);
 
-  extx=(circ-space)*(sparse(1:mnx(1),mnx(2)-indx+1,1));
-  exty=(circ-space)*(sparse(1:mny(1),mny(2)-indy+1,1));
+    extx=(circ-space)*(sparse(1:mnx(1),mnx(2)-indx+1,1));
+    exty=(circ-space)*(sparse(1:mny(1),mny(2)-indy+1,1));
 
-  if ~isempty(XTL) && swit~=2
-    XTL=str2mat(abs(XTL)+extx); 
-    % For latest Matlab; after this, do $UFILES/degs
-    % Actually, this doesn't help
-    % XTL=[repmat(' ',size(XTL,1),1) XTL];
-    % HOW ABOUT setstr(hex2dec('B0')) THIS DOESN'T HELP EITHER
+    if ~isempty(XTL) && swit~=2
+      XTL=str2mat(abs(XTL)+extx); 
+      % Actually, this doesn't help
+      % XTL=[repmat(' ',size(XTL,1),1) XTL];
+      % HOW ABOUT setstr(hex2dec('B0')) THIS DOESN'T HELP EITHER
+    end
+    if ~isempty(YTL) && swit~=1 
+      YTL=str2mat(abs(YTL)+exty); 
+      % Actually, this doesn't help
+      % YTL=[repmat(' ',size(YTL,1),1) YTL];
+    end
+  else
+    if ~isempty(XTL) && swit~=2
+      for ondex=1:size(XTL,1)
+        XTL{ondex}=sprintf('%s%s',XTL{ondex},circ);
+      end
+    end
+    if ~isempty(YTL) && swit~=1 
+      for ondex=1:size(YTL,1)
+        YTL{ondex}=sprintf('%s%s',YTL{ondex},circ);
+      end
+    end
   end
-  if ~isempty(YTL) && swit~=1 
-    YTL=str2mat(abs(YTL)+exty); 
-    % For latest Matlab; after this, do $UFILES/degs
-    % Actually, this doesn't help
-    % YTL=[repmat(' ',size(YTL,1),1) YTL];
-  end
-  
+  % Effectuate the switch
   switch swit
     case 1
       set(ah(index),'XTickLabel',XTL)
