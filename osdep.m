efab01df7820192cf45fa04c7c9f95c190e0ec60
Frederik J. Simons <fjsimons@gmail.com>
2020-03-13 10:15:47 -0400

    Sets read flag according to the operating system

--- osdep.m
+++ osdep.m
@@ -5,11 +5,16 @@ function of=osdep
 % on the LOCAL operating system for files created
 % on the LOCAL operating system.
 %
+% OUTPUT:
+%
+% of      Said value
+%
 % Last modified by fjsimons-at-alum.mit.edu, 06/16/2019
 
 defval('of','l')
 
-if strcmp(getenv('OSTYPE'),'linux')
+% Modified by jdsimon-at-princeton.edu on 11/15/2014 for MAC
+if strcmp(getenv('OSTYPE'),'linux') || isunix==1
   of= 'l';  
 end
 if strcmp(getenv('OSTYPE'),'solaris')
