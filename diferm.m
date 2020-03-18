502521e2012e63d3ce27b5058eff8c021cd214af
Frederik J. Simons <fjsimons@gmail.com>
2020-03-17 22:30:00 -0400

    Mute form of DIFER to compare two quantities

--- diferm.m
+++ diferm.m
@@ -1,13 +1,23 @@
-function diferm(a,b,tolex)
-% DIFERM(a,b,tolex)
+function diferm(a,b,tolex,sev)
+% DIFERM(a,b,tolex,sev)
 % 
-% The mute form of DIFER, i.e.
-% difer(a,[],[],NaN)
-% difer(a-b,[],[],NaN)
-% difer(a-b,tolex,[],NaN)
+% The mute form of DIFER, i.e. difer(a-b,tolex,sev,NaN)
 %
-% Last modified by fjsimons-at-alum.mit.edu, 11/15/2016
+% a         The first vector offered for comparison
+% b         The second vector offered for comparison
+% tolex     Tolerance exponent (default: 10 for 1e-10)
+% sev       0 produces WARNING upon failure [default]
+%           1 produces ERROR upon failure 
+%           2 invokes KEYBOARD upon failure
+%
+% Last modified by fjsimons-at-alum.mit.edu, 03/20/2020
 
+% Any second input
 defval('b',0)
+% The tolerance exponent
 defval('tolex',[])
-difer(a-b,tolex,[],NaN)
+% The severity index
+defval('sev',[])
+
+% Run the test
+difer(a-b,tolex,sev,NaN)
