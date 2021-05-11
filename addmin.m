91b13a567f633d7ef179c6bfa19a92875b359d4d
Frederik J. Simons <fjsimons@gmail.com>
2021-05-11 11:29:25 -0400

    Repeats the entries in a column vector the row number of times

--- addmin.m
+++ addmin.m
@@ -1,20 +1,26 @@
 function vec=addmin(invec)
 % vec=ADDMIN(invec)
 %
-% Repeats the entries of an input vector INDEX times
-% where INDEX is the array index, and constructs a new vector with these
-% repeated array elements (these could be numbers or strings).
+% Repeats the entries of a column vector the row number of times.
 % Useful for real spherical harmonics. 
 %
-% Last modified by fjsimons-at-alum.mit.edu, Jan 27th, 2003
+% INPUT: 
+%
+% invec       Input array with numbers or strings
+%
+% OUTPUT:
+%
+% vec         Output array with repeated entries
+%
+% Last modified by fjsimons-at-alum.mit.edu, 05/11/2021
 
+% Ensure column vector input
 invec=invec(:);
 
+% Think - maximum spherical harmonic degree
 L=size(invec,1)-1;
 
-el=1:L+1;
+% So this function is really just an interface to GAMINI
+vec=gamini(invec,1:L+1);
 
-vec=gamini(invec,el);
-
-vec=vec(:);
 
