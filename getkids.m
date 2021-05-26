979a2ae447a1170bdc78c25d2655ede993fedb7a
Frederik J. Simons <fjsimons@gmail.com>
2021-05-26 12:13:29 -0400

    Returns children of a graphics object

--- getkids.m
+++ getkids.m
@@ -1,13 +1,18 @@
-function kids=getkids(aks,varargin)
-% kids=GETKIDS(ah)
+function kids=getkids(ah,varargin)
 % kids=GETKIDS(ah,ind)
 %
 % Get axis handle's children handles.
 %
-% Last modified by fjsimons-at-alum.mit.edu, June 10th, 2004
+% INPUT:
+%
+% ah       An axis handle
+% ind      An index of you want a particular one
+%
+% Last modified by fjsimons-at-alum.mit.edu, 05/26/2021
 
-kids=get(aks,'Children');
+kids=get(ah,'Children');
 
+% Subselection if you know what you want
 if nargin>1
   kids=kids(varargin{1});
 end
