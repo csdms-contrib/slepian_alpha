eff52955204d71ddebbcc752e9f71a2275511cb9
Frederik J. Simons <fjsimons@gmail.com>
2021-05-26 16:32:16 -0400

    Plots an array as greyscale symbols on a geographical map

--- plotprop.m
+++ plotprop.m
@@ -1,14 +1,14 @@
 function varargout=plotprop(lon,lat,prop,bnd,axl,ms,mrk,clox)
-% PLOTPROP(lon,lat,prop,bnd,axl,ms,mrk,clox)
-% [ah,cb,p,pc,pp]=PLOTPROP(...)
+% [ah,cb,p,pc,pp]=PLOTPROP(lon,lat,prop,bnd,axl,ms,mrk,clox)
 %
-% Plots the third dimension as color-coded symbols on a map.
+% Plots properties as grey-scale symbols on a geographical map implemented
+% as a two-dimensional view of a three-dimensional rendition
 %
 % INPUT:
 %
-% lon          Longitude (x-coordinate)
-% lat          Latitude (y-coordinate)
-% dep          Depth or some other property
+% lon          Longitude array
+% lat          Latitude array
+% prop         Some property associated with lon and lat
 % bnd          Color boundaries [default: 10 bins]
 % axl          Axis limits
 % ms           Marker size [default: 6]
@@ -34,9 +34,11 @@ function varargout=plotprop(lon,lat,prop,bnd,axl,ms,mrk,clox)
 % plotprop(linspace(100,200,12),repmat(0,1,12),...
 %          [1:12]+0.1,1:2:20,[90 210 -10 10],15)
 %
-% SEE ALSO: PLOTEQ
+% SEE ALSO: PLOTPROPS
 %
-% Last modified by fjsimons-at-alum.mit.edu, 07/07/2008
+% TESTED ON: MATLAB Version: 8.3.0.532 (R2014a)
+%
+% Last modified by fjsimons-at-alum.mit.edu, 05/26/2021
 
 % Supply defaults
 defval('ms',6)
@@ -49,17 +51,17 @@ defval('clox','hor');
 
 lon=lon+360*(lon<0);
 
-% California
+% So it's a flattened 3D thing
 ah=gca;
 for index=1:length(lon)
   p(index)=plot3(lon(index),lat(index),index,mrk);
   hold on
 end
-
 view(2)
+% It's an actual geographical thing
 [a,b]=plotcont;
 c=plotplates;
-
+hold off
 axis image
 axis(axl)
 
@@ -71,7 +73,7 @@ if min(prop)<=bnd(1)
 end
 % Use inequalities & find out in which bin the data are
 for index=1:length(bnd)
-  propco(prop>bnd(index))=length(bnd)-index+1;
+  propco(prop>=bnd(index))=length(bnd)-index+1;
 end
 
 % The grey scale, of course, is from 0 to 1
@@ -90,27 +92,26 @@ cb=colorbar(clox);
 colormap(flipud(gray(length(bnd))))
 caxis([0 1])
 
+% Meaningful colorbar labels 
 for index=1:length(bnd)
   bon{index}=sprintf('>%i',round(bnd(index)));
 end
 
+% New for this version
+els=kindeks(linspace(0,1,length(bon)+1),2:length(bon)+1);
+els=els-[els(2)-els(1)]/2;
 switch clox
  case 'hor'
-  set(cb,'XTick',1.5:length(bnd)+0.5,'XTickL',bon)
+  set(cb,'XTick',els,'XTickLabel',bon)
  case 'ver'
-  set(cb,'YTick',1.5:length(bnd)+0.5,'YTickL',bon)
-  otherwise
+  set(cb,'YTick',els,'YTickLabel',bon)
+ otherwise
   error('Specify a legal colorbar orientation')
 end
 
-set(cb,'TickDir','out','TickLength',[0.02 0.025]/3)
-
+longticks(cb,3)
 longticks(ah,2)
 
+% Optional output
 varns={ah,cb,p,b,c};
 varargout=varns(1:nargout);
-
-hold off
-
-
-
