78e3c9dc27579ed5f3b38f82349a9f129b9b3249
Frederik J. Simons <fjsimons@gmail.com>
2021-05-26 12:32:52 -0400

    Transforms edge coordinates to the 'extent' of a box

--- lrtb2ext.m
+++ lrtb2ext.m
@@ -1,6 +1,10 @@
 function ext=lrtb2ext(lrtb)
 % ext=LRTB2EXT(obj)
 %
+% Transforms (left-right-top-bottom) 'edge positions' of a graphics
+% object into an equivalent box 'extent' (left-bottom-width-height)
+
+% 
 % INPUT:
 %
 % lrtb      Coordinates (left-right-top-bottom)
@@ -11,8 +15,6 @@ function ext=lrtb2ext(lrtb)
 %
 % See also FILLBOX, ALLEN1, EXT2LRTB
 % 
-% Last modified by fjsimons-at-alum.mit.edu, 10/22/2012
+% Last modified by fjsimons-at-alum.mit.edu, 05/26/2021
 
 ext=[lrtb(1) lrtb(4) lrtb(2)-lrtb(1) abs(lrtb(4)-lrtb(3))];
-
-
