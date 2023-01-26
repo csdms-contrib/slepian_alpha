63a2b68e9ab1b5c229117db3e022dd0df4e7364e
Frederik J. Simons <fjsimons@gmail.com>
2023-01-26 11:49:28 -0500

    Coordinates of the eponymous regions

---
+++ REGIONS/france.m
@@ -0,0 +1,43 @@
+function varargout=france(res,buf)
+% XY=FRANCE(res,buf)
+% FRANCE(...) % Only makes a plot
+%
+% Finds the coordinates of France, potentially buffered by some amount.
+%
+% INPUT:
+%
+% res      0 The standard, default values
+%          N Splined values at N times the resolution
+% buf      Distance in degrees that the region outline will be enlarged
+%          by BUFFERM, not necessarily integer, possibly negative
+%          [default: 0]
+%
+% OUTPUT:
+%
+% XY       Closed-curved coordinates of the continent
+%
+% Last modified by fjsimons-at-alum.mit.edu, 01/26/2023
+
+defval('res',0)
+defval('buf',0)
+
+% Parameters that make this the region in question
+regn='france';
+c11=[-10 50];
+cmn=[10 30];
+xunt=[];
+
+% This admittedly is a special case
+XY=load(fullfile(getenv('IFILES'),'COASTS','france'));
+
+% Do it! Make it, load it, save it
+XY=regselect(regn,XY(:,1),XY(:,2),xunt,res,buf);
+
+if nargout==0
+  plot(XY(:,1),XY(:,2),'k-'); axis image; grid on
+end
+
+% Prepare optional output
+varns={XY};
+varargout=varns(1:nargout);
+
