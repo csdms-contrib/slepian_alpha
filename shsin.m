1d3331b37ce7581335cea6f16d7f24f2f0eeb5a1
Frederik J. Simons <fjsimons@gmail.com>
2023-06-15 09:50:54 -0400

    Picks sine coefficients out of a spherical harmonic array

--- shsin.m
+++ shsin.m
@@ -13,7 +13,7 @@ function [S,b,e]=shsin(lmcosi,l)
 %
 % See also SHCOS
 %
-% Last modified by fjsimons-at-alum.mit.edu, Feb 16th 2004
+% Last modified by fjsimons-at-alum.mit.edu, 06/15/2023
 
 % Vector or matrix?
 if size(lmcosi,2)==1
